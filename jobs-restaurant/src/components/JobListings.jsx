2e012846897ae142302aed97a5b2e6cf20139f44
lucianN9 <lucian.nutu09@gmail.com>
2024-08-31 14:45:55 +0300

    Changed the limit of jobs cards from 3 to 2 on Home page

--- jobs-restaurant/src/components/JobListings.jsx
+++ jobs-restaurant/src/components/JobListings.jsx
@@ -8,7 +8,7 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
-      const apiUrl = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+      const apiUrl = isHome ? "/api/jobs?_limit=2" : "/api/jobs";
       try {
         const res = await fetch(apiUrl);
         const data = await res.json();
@@ -27,13 +27,13 @@ const JobListings = ({ isHome = false }) => {
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-blue-900 mb-6 text-center">
-          {isHome ? "Recent Jobs" : "Browse Jobs"}
+          {isHome ? "Newly Opened" : "Browse Jobs"}
         </h2>
 
         {loading ? (
           <Spinner loading={loading} />
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {jobs.map((job) => (
               <JobListing key={job.id} job={job} />
             ))}
